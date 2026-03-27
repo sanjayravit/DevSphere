6b4a966a3b6effdf76e5ccfd74c8e9d08443cd6e
sanjay <sanjayravit7@gmail.com>
2026-03-27 14:57:39 +0530

    g

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,18 +1,16 @@
 const admin = require("firebase-admin");
 
-if (!admin.apps.length) {
-    const serviceAccount = process.env.NODE_ENV === "production"
-        ? {
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: (process.env.FIREBASE_PRIVATE_KEY || "").replace(/\\n/g, '\n').replace(/^"|"$/g, ''),
-        }
-        : require("./serviceAccountKey.json");
+const serviceAccount = process.env.NODE_ENV === "production"
+    ? {
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }
+    : require("./serviceAccountKey.json");
 
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-    });
-}
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
 
 const db = admin.firestore();
 
